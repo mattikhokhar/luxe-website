d255fe5429ae2d507e7e9dc9fed7d0689b89edc2
mattikhokhar <mattikhokhar009@gmail.com>
2026-03-16 08:15:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showMessage(){
+
+alert("Welcome to Super AI Website!");
+
+}
+
+function chatBot(){
+
+let question = prompt("Ask something:");
+
+if(question){
+
+alert("Thanks! We will answer soon.");
+
+}
+
+}
